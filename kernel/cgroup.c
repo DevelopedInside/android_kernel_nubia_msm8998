13af1ce772830538761687df7307e287a4353cfc
BeYkeRYkt <beykerykt@gmail.com>
2018-09-25 02:25:55 +0900

    kernel: import nubia_cgf_notify
    Change-Id: Icf5098d721263f54d0ada86e36dc755867347f8a

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -59,6 +59,9 @@
 #include <linux/delay.h>
 #include <linux/cpuset.h>
 #include <linux/atomic.h>
+#ifdef CONFIG_NUBIA_CGF_NOTIFY_EVENT
+#include <linux/notifier.h>
+#endif
 
 /*
  * pidlists linger the following amount before being destroyed.  The goal
@@ -2826,6 +2829,104 @@ static ssize_t cgroup_tasks_write(struct kernfs_open_file *of,
 {
 	return __cgroup_procs_write(of, buf, nbytes, off, false);
 }
+
+#ifdef CONFIG_NUBIA_CGF_NOTIFY_EVENT
+static int attach_task_by_pid(struct cgroup *cgrp, u64 pid, bool threadgroup)
+{
+	struct task_struct *tsk;
+	const struct cred *cred = current_cred();
+	const struct cred *tcred;
+	struct cgroup_subsys *ss;
+	int ssid=0;
+	int ret=0;
+
+	if (pid < 0)
+		return -EINVAL;
+
+	if (!cgroup_tryget(cgrp))
+		return -ENODEV;
+
+	mutex_lock(&cgroup_mutex);
+	if (cgroup_is_dead(cgrp)) {
+		mutex_unlock(&cgroup_mutex);
+		cgroup_put(cgrp);
+		return -ENODEV;
+	}
+
+	percpu_down_write(&cgroup_threadgroup_rwsem);
+	rcu_read_lock();
+	if (pid) {
+		tsk = find_task_by_vpid(pid);
+		if (!tsk) {
+			ret = -ESRCH;
+			goto out_unlock_rcu;
+		}
+	} else {
+		tsk = current;
+	}
+
+	if (threadgroup)
+		tsk = tsk->group_leader;
+
+	if (tsk->no_cgroup_migration || (tsk->flags & PF_NO_SETAFFINITY)) {
+		ret = -EINVAL;
+		goto out_unlock_rcu;
+	}
+
+	get_task_struct(tsk);
+	rcu_read_unlock();
+
+	tcred = get_task_cred(tsk);
+	if (!uid_eq(cred->euid, GLOBAL_ROOT_UID) &&
+	    !uid_eq(cred->euid, tcred->uid) &&
+	    !uid_eq(cred->euid, tcred->suid) &&
+	    !ns_capable(tcred->user_ns, CAP_SYS_NICE))
+		ret = -EACCES;
+	if (!ret && cgroup_on_dfl(cgrp)) {
+		struct cgroup *cgrp_tmp;
+
+		spin_lock_irq(&css_set_lock);
+		cgrp_tmp = task_cgroup_from_root(tsk, &cgrp_dfl_root);
+		spin_unlock_irq(&css_set_lock);
+		while (!cgroup_is_descendant(cgrp, cgrp_tmp))
+			cgrp_tmp = cgroup_parent(cgrp_tmp);
+	}
+
+	put_cred(tcred);
+	if (!ret)
+		ret = cgroup_attach_task(cgrp, tsk, threadgroup);
+	put_task_struct(tsk);
+	goto out_unlock_threadgroup;
+
+out_unlock_rcu:
+	rcu_read_unlock();
+out_unlock_threadgroup:
+	percpu_up_write(&cgroup_threadgroup_rwsem);
+	for_each_subsys(ss, ssid)
+		if (ss->post_attach)
+			ss->post_attach();
+	mutex_unlock(&cgroup_mutex);
+	cgroup_put(cgrp);
+	return ret;
+}
+int cgf_attach_task_group(struct cgf_event *event)
+{
+	struct freezer *freezer = container_of(event, struct freezer, event);
+	struct task_struct *p;
+	int ret = 0;
+
+	if(!freezer->event.data) {
+		ret = -EINVAL;
+		goto out_invalid_data;
+	}
+	p = freezer->event.data;
+	ret = attach_task_by_pid(freezer->css.cgroup, p->pid, true);
+out_invalid_data:
+	return ret;
+}
+EXPORT_SYMBOL_GPL(cgf_attach_task_group);
+#endif
+
 
 static ssize_t cgroup_procs_write(struct kernfs_open_file *of,
 				  char *buf, size_t nbytes, loff_t off)
