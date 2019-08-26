78e7303d0272af8d10c44f0b5874b19694c110cb
Cyborg2017 <wangyang0987654@gmail.com>
2019-08-26 18:06:50 +0800

    drivers: clk: Import nubia changes

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -2314,7 +2314,13 @@ EXPORT_SYMBOL_GPL(clk_set_flags);
 
 static struct dentry *rootdir;
 static int inited = 0;
+//Begin [0016004715 add the kernel power code,20180316]
+#ifdef CONFIG_ZTEMT_POWER_DEBUG
+static u32 debug_suspend = 1;
+#else
 static u32 debug_suspend;
+#endif //CONFIG_ZTEMT_POWER_DEBUG
+//End [0016004715 add the kernel power code,20180316]
 static DEFINE_MUTEX(clk_debug_lock);
 static HLIST_HEAD(clk_debug_list);
 
