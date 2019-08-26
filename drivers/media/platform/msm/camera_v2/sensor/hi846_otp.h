25874ca272132afe9046c0edbf56ca3478db313c
Cyborg2017 <wangyang0987654@gmail.com>
2019-08-26 18:14:28 +0800

    drivers: media: platform: Import nubia sepcific camera_v2 driver(al3200)

---
+++ drivers/media/platform/msm/camera_v2/sensor/hi846_otp.h
@@ -0,0 +1,16 @@
+/* Copyright (c) 2011-2016, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+ //ZTEMT: zhouruoyu add for hi846 otp ---start
+int32_t hi846_otp_func(struct msm_sensor_ctrl_t *s_ctrl);
+void hi846_otp_wb_calculation(struct msm_sensor_ctrl_t *s_ctrl);
+ //ZTEMT: zhouruoyu add for hi846 otp ---end
+
